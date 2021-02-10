3653410e4cdae3918c33d328da7fdca7966298a4
Dennier Agreda <dennioagreda@gmail.com>
2021-02-09 22:16:42 -0500

    quiz

---
+++ Quiz.R
@@ -0,0 +1,83 @@
+##########################################################
+# Create edx set, validation set (final hold-out test set)
+##########################################################
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+library(data.table)
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+
+# if using R 4.0 or later:
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(movieId),
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Validation set will be 10% of MovieLens data
+set.seed(1, sample.kind="Rounding") # if using R 3.5 or earlier, use `set.seed(1)`
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in validation set are also in edx set
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+# Pta: 1.1
+dim(edx)
+# Pta: 1.2
+dim(edx[edx$rating==3,])
+
+length(unique(edx$movieId))
+length(unique(edx$userId))
+
+drama <- edx %>% filter(str_detect(genres,"Drama"))
+comedy <- edx %>% filter(str_detect(genres,"Comedy"))
+thriller <- edx %>% filter(str_detect(genres,"Thriller"))
+romance <- edx %>% filter(str_detect(genres,"Romance"))
+
+edx %>% group_by(title) %>% summarise(number = n()) %>%
+  arrange(desc(number)) %>% 
+  top_n(5)
+
+edx %>% group_by(rating) %>% 
+  summarize(number = n()) %>% 
+  top_n(5) %>% 
+  arrange(desc(number))
+
+numRatings %>%
+  mutate(halfStar = rating %% 1 == 0.5) %>%
+  group_by(halfStar) %>%
+  summarize(number = sum(number))
+
+edx %>%
+  group_by(rating) %>%
+  summarize(count = n()) %>%
+  ggplot(aes(x = rating, y = count)) +
+  geom_line()
